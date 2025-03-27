3812f96a77a9fc920d7f600808ccbda0bbe69a14
Sempi18 <Sempi18@users.noreply.github.com>
2025-03-27 20:33:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ITS-ProjectoIonic-LucasDuran',
+  webDir: 'www'
+};
+
+export default config;
